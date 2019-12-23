b0c9cbff0018a8e31028578f12e9fb947d4000e6
Oksana Dionisieva <dioksa@Dioksa-MacBook.local>
2019-12-23 11:41:53 +0200

    MTP-2 Add main screen
    Add main screen with buttons

--- MapTestProject/ViewController.swift
+++ MapTestProject/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var topButton: UIButton! {
+        didSet {
+            UIView.animate(withDuration: 1, animations: { [weak self] in
+                self?.topButton.backgroundColor = .red
+                self?.topButton.frame.size.width += 40
+                self?.topButton.frame.size.height += 40
+            }) { _ in
+                UIView.animate(withDuration: 1, delay: 0.25, options: [.autoreverse], animations: { [weak self] in
+                    self?.topButton.frame.origin.y -= 20
+                })
+            }
+        }
+    }
+    
+    @IBOutlet weak var bottomButton: UIButton! {
+        didSet {
+            UIView.animate(withDuration: 1, animations: { [weak self] in
+                self?.bottomButton.backgroundColor = .red
+                self?.bottomButton.frame.size.width -= 40
+                self?.bottomButton.frame.size.height -= 40
+            }) { _ in
+                UIView.animate(withDuration: 1, delay: 0.25, options: [.autoreverse], animations: { [weak self] in
+                    self?.bottomButton.frame.origin.y += 20
+                })
+            }
+        }
+    }
+    
+    @IBAction func topButtonAction(_ sender: UIButton) {
+        print("Top button was tapped")
+    }
+    
+    
+    @IBAction func bottomButtonAction(_ sender: UIButton) {
+        print("Button button was tapped")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
